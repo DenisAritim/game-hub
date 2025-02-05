5440f756613eddccdb4ad1069f48c7676313d7eb
DenisParise <dd.parise@gmail.com>
2025-02-05 15:03:43 +0100

    refactoring, implemented fetching with react query

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+    count: number;
+    results: T[];
+}
+
 export default axios.create({
-  baseURL: "https://api.rawg.io/api",
-  params: {
-    key: "82bb7853180e44d3a6dc69b378a15c33",
-  },
+    baseURL: "https://api.rawg.io/api",
+    params: {
+        key: "82bb7853180e44d3a6dc69b378a15c33",
+    },
 });
