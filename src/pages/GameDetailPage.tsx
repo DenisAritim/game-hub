e3102dcda1baae19a504732714c970860b7ac138
DenisParise <dd.parise@gmail.com>
2025-02-09 12:50:14 +0100

    built game attributes

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,7 @@
 import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
+import GameAttributes from "../components/GameAttributes";
 import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
@@ -9,16 +10,13 @@ const GameDetailPage = () => {
 
     if (isLoading) return <Spinner />;
 
-    if (error) throw error;
+    if (error || !game) throw error;
 
     return (
         <>
             <Heading>{game?.name}</Heading>
-            <ExpandableText>
-                {game?.description_raw
-                    ? game.description_raw
-                    : "No description avaliable"}
-            </ExpandableText>
+            <ExpandableText>{game.description_raw}</ExpandableText>
+            <GameAttributes game={game} />
         </>
     );
 };
